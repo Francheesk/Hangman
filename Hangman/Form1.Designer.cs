ed38dd3e7a5ac7d52b5fb56bdffa3753c3d4e874
Tulici Francisc <francisctulici@yahoo.com>
2019-06-20 13:52:54 +0300

    Initial Commit

---
+++ Hangman/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.checkAnimale = new System.Windows.Forms.CheckBox();
+            this.checkAdjective = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(400, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(539, 51);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(576, 147);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(97, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Nivel Incepator";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(576, 96);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "Nivel Avansat";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 96);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(319, 428);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // checkAnimale
+            // 
+            this.checkAnimale.AutoSize = true;
+            this.checkAnimale.Location = new System.Drawing.Point(576, 318);
+            this.checkAnimale.Name = "checkAnimale";
+            this.checkAnimale.Size = new System.Drawing.Size(63, 17);
+            this.checkAnimale.TabIndex = 8;
+            this.checkAnimale.Text = "Animale";
+            this.checkAnimale.UseVisualStyleBackColor = true;
+            // 
+            // checkAdjective
+            // 
+            this.checkAdjective.AutoSize = true;
+            this.checkAdjective.Location = new System.Drawing.Point(576, 354);
+            this.checkAdjective.Name = "checkAdjective";
+            this.checkAdjective.Size = new System.Drawing.Size(70, 17);
+            this.checkAdjective.TabIndex = 9;
+            this.checkAdjective.Text = "Adjective";
+            this.checkAdjective.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(576, 387);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(576, 424);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 11;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(679, 560);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkAdjective);
+            this.Controls.Add(this.checkAnimale);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "g";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox checkAnimale;
+        private System.Windows.Forms.CheckBox checkAdjective;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ Hangman/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        Bitmap DrawArea;
+        Bitmap hangmanImage;
+        char[] alfabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        string[] beginnerWords = File.ReadAllLines("file.txt", Encoding.UTF8);
+        string[] advancedWords = { "PARALELIPIPED", "INTANGIBIL", "AVANSAT" };
+        int beginnerHints = 2;
+        int advancedHints = 3;
+        List<char> solved = new List<char>();
+        string temporar;
+        int hangmanCounter = 1;
+        string concatenat;
+        string word;
+        char[] wordLetters;
+        bool start=false;
+        public Form1()
+        {
+            InitializeComponent();
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = DrawArea;
+            hangmanImage = new Bitmap(pictureBox2.Size.Width, pictureBox2.Size.Height);
+            pictureBox2.Image = hangmanImage;
+            this.KeyPreview = true;
+    
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            start = true;
+            Graphics graphic = Graphics.FromImage(pictureBox1.Image);
+            graphic.Clear(Color.White);
+            solved.Clear();
+            wordLetters = null;
+            hangmanCounter = 1;
+
+            if (radioButton1.Checked)
+            {
+                word= alesRandom(beginnerWords, beginnerHints);
+            }
+            else {
+               word= alesRandom(advancedWords, advancedHints);
+            }
+
+            wordLetters = word.ToCharArray();
+            temporar = word;
+            
+            printWord(wordLetters);
+           
+            drawHangman(1);
+
+        }
+
+        
+        private void printWord(char[] word)
+        {
+            Graphics g;
+            g = Graphics.FromImage(DrawArea);
+
+            Font myFont = new System.Drawing.Font("Helvetica", 15);
+            Brush myBrush = new SolidBrush(System.Drawing.Color.Black);
+            int dist = 0;
+            foreach (char letter in word)
+           {
+                if(solved.Contains(letter))
+                {
+                    g.DrawString(letter.ToString(), myFont, myBrush, dist, 15);
+                } else
+                {
+                    g.DrawString("_", myFont, myBrush, dist, 15);
+                }
+                
+                dist = dist + 40;
+            }
+            pictureBox1.Image = DrawArea;
+            g.Dispose();
+        }
+        private string alesRandom(string[] cuvinte, int hinturi)
+        {
+
+            Random rnd = new Random();
+            string word;
+            int ales;
+            ales = rnd.Next(cuvinte.Length);
+            word = cuvinte[ales];
+            char[] wordLetters = word.ToCharArray();
+            for (int i = 1; i <= hinturi; i++)
+            {
+                ales = rnd.Next(word.Length);
+                solved.Add(wordLetters[ales]);
+            }
+            return word;
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawHangman(int counter)
+        {
+         
+            concatenat = String.Concat(counter, ".png");
+            pictureBox2.Load(concatenat);
+          
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool esteLiterabuna = false;
+            if (!start){
+                MessageBox.Show("N-ati dat start", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            if ((e.KeyChar >= 'a' && e.KeyChar <= 'z' )|| (e.KeyChar >= 'A' && e.KeyChar <= 'Z'))
+            {
+                if (solved.Contains(Char.ToUpper(e.KeyChar)))
+                {
+                    MessageBox.Show("Litera ii deja in cuvant", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                    
+                    return;
+                }
+                for (int i = 0; i < wordLetters.Length; i++)
+                {
+
+                    if (Char.ToUpper(e.KeyChar) == wordLetters[i])
+                    {
+                       
+                        
+                            solved.Add(Char.ToUpper(e.KeyChar));
+                            printWord(wordLetters);
+                            esteLiterabuna = true;
+                        
+                    }
+                }
+                if (!esteLiterabuna)
+                {
+                    hangmanCounter++;
+                    drawHangman(hangmanCounter);
+                }
+               
+            }
+          
+            else
+            {
+                
+                MessageBox.Show("Ati introdus o nonlitera, " + e.KeyChar, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            if (hangmanCounter == 7) {
+                MessageBox.Show("Ati pierdut jocul, apasati ok pentru a reincepe" , "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                button1.PerformClick();
+            }
+            e.Handled = true;
+           
+        }
+    }
+}
